d38db48cc82bc4b4465de23c6c0cb7270315e7a0
lauralardies <lrodrlar@myuax.com>
2022-03-24 08:46:56 +0100

    solidificamos la estructura

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -12,12 +12,22 @@ class Banco:
         if dinero_retirar > self.saldo:
             print("No puedes retirar esta cantidad dinero de la cuenta.")
             exit
+        else: 
+            self.saldo = self.saldo - dinero_retirar
+            print("Se han retirado " + str(dinero_retirar) + "€ a tu cuenta bancaria.")
 
     def ingresar_dinero(self):
+        dinero_ingresar =
+        self.saldo = dinero_ingresar + self.saldo
+        print("Se han añadido " + str(dinero_ingresar) + "€ a tu cuenta bancaria.")
 
     def transferir_dinero(self):
         dinero_transferir = 
         cuenta_transferir =
         if dinero_transferir > self.saldo:
-            print("No puedes transferir esta cantidad dinero a otra cuenta cuenta.")
+            print("No puedes transferir esta cantidad dinero a otra cuenta.")
             exit
+        else:
+            self.saldo = self.saldo - dinero_transferir
+            print("Se han transferido " + str(dinero_transferir) + "€ a la cuenta " + str(cuenta_transferir))
+
