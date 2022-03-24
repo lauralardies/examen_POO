565d5e53e9d257d384519892db26b69de6e60239
lauralardies <lrodrlar@myuax.com>
2022-03-24 08:42:43 +0100

    estructura ej 4

---
+++ ejercicio_4.py
@@ -0,0 +1,23 @@
+class Banco:
+
+    def __init__(self) -> None:
+        self.id =
+        self.nombre_titular =
+        self.fecha_apertura = 
+        self.num_cuenta = 
+        self.saldo = 
+    
+    def retirar_dinero(self):
+        dinero_retirar = 
+        if dinero_retirar > self.saldo:
+            print("No puedes retirar esta cantidad dinero de la cuenta.")
+            exit
+
+    def ingresar_dinero(self):
+
+    def transferir_dinero(self):
+        dinero_transferir = 
+        cuenta_transferir =
+        if dinero_transferir > self.saldo:
+            print("No puedes transferir esta cantidad dinero a otra cuenta cuenta.")
+            exit
