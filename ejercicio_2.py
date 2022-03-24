575541fdb63fc3f224352a8a833888fcd19deb32
lauralardies <lrodrlar@myuax.com>
2022-03-24 10:13:35 +0100

    init de libro

---
+++ ejercicio_2.py
@@ -0,0 +1,8 @@
+class Libro:
+    def __init__(self) -> None:
+        self.paginas = 30 # Este libro tiene 30 páginas
+        self.tapa = "Tapa dura" # Los libros pueden tener tapa dura o blanca, este tiene dura
+        self.genero = "Fantasía" # Los libros pertenecen a un género específico
+        self.titulo = "Las crónicas de Narnia" # Los libros tienen un título
+        self.autor = "C.S. Lewis" # Los libros tienen un autor
+        self.idioma = "Inglés" # Los libros están en un idioma
